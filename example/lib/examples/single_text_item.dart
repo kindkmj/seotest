9d061dd2b106dc9c585854094494c42199f31352
minjin <kindcode@gmail.com>
2022-03-21 10:42:16 +0900

    d
    d

---
+++ example/lib/examples/single_text_item.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:seo_renderer/seo_renderer.dart';
+
+class SingleTextItem extends StatelessWidget {
+  const SingleTextItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+          child: Column(
+        children: [
+          TextRenderer(
+            style: TextRendererStyle.header1,
+            child: Text('Heading element <H1>'),
+          ),
+          TextRenderer(
+            style: TextRendererStyle.header2,
+            child: Text('Heading element <H2> and etc to h6'),
+          ),
+          TextRenderer(
+            style: TextRendererStyle.paragraph,
+            child: Text(
+              'Paragraph <p>: Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+            ),
+          ),
+        ],
+      )),
+    );
+  }
+}
---
+++ example/lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:seo_renderer/seo_renderer.dart';
+import 'package:seo_renderer_example/examples/image_renderer_example.dart';
+import 'package:seo_renderer_example/examples/link_singletext_example.dart';
+import 'package:seo_renderer_example/examples/scrollable_content.dart';
+import 'package:seo_renderer_example/examples/single_text_item.dart';
+import 'package:seo_renderer_example/examples/visibility_example.dart';
+
+void main() {
+  runApp(
+    RobotDetector(
+      child: MaterialApp(
+        home: MyApp(),
+        navigatorObservers: [seoRouteObserver],
+      ),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            OutlinedButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (_) => SingleTextItem()));
+              },
+              child: TextRenderer(
+                child: Text('Single Text Item'),
+              ),
+            ),
+            OutlinedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (_) => ScrollableContent()));
+              },
+              child: TextRenderer(
+                child: Text('Scrollable Text Content'),
+              ),
+            ),
+            OutlinedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (_) => SingleTextLinkExample()));
+              },
+              child: TextRenderer(
+                child: Text('Single Link Text Item'),
+              ),
+            ),
+            OutlinedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (_) => ImageRendererExample()));
+              },
+              child: TextRenderer(
+                child: Text('Image renderer'),
+              ),
+            ),
+            OutlinedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (_) => VisibilityExample()));
+              },
+              child: TextRenderer(
+                child: Text('Visibility'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
